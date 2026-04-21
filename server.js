f839b022bbaea7be0ebc79fdfb6a7e448af6009a
barky28 <ilhmnrf@gmail.com>
2026-04-21 11:16:52 +0700

    fix syntax error

--- server.js
+++ server.js
@@ -4,21 +4,15 @@ const { exec } = require("child_process");
 const fs = require("fs");
 
 const app = express();
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
-}));
 
-app.use(cors({
-  origin: "*"
-}));
-
+app.use(cors());
 app.use(express.json());
-app.use("/frames", express.static("frames"));
+
 if (!fs.existsSync("frames")) {
   fs.mkdirSync("frames");
 }
+
+app.use("/frames", express.static("frames"));
 
 app.get("/", (req, res) => {
   res.send("Server hidup");
@@ -31,52 +25,41 @@ app.post("/generate", (req, res) => {
     return res.json({ error: "URL kosong" });
   }
 
+  console.log("Download video...");
+
   exec(`yt-dlp -f "bv*+ba/b" -o video.mp4 ${url}`, (err, stdout, stderr) => {
-  if (err) {
-    console.log("YTDLP ERROR:", stderr);
-    return res.json({ error: "Download gagal" });
-  }
-
-  console.log("Download sukses");
-
-  // hapus frame lama
-  fs.readdirSync("frames").forEach(file => {
-    fs.unlinkSync(`frames/${file}`);
-  });
-
-  exec(`ffmpeg -i video.mp4 -vf fps=1 frames/frame_%03d.jpg`, (err, stdout, stderr) => {
     if (err) {
-      console.log("FFMPEG ERROR:", stderr);
-      return res.json({ error: "FFmpeg gagal" });
+      console.log("YTDLP ERROR:", stderr);
+      return res.json({ error: "Download gagal" });
     }
 
-    console.log("FFmpeg sukses");
+    console.log("Ambil frame...");
 
-    const files = fs.readdirSync("frames");
-
-    const urls = files.map(file => {
-      return `https://video-to-image-production.up.railway.app/frames/${file}`;
+    // hapus frame lama
+    fs.readdirSync("frames").forEach(file => {
+      fs.unlinkSync(`frames/${file}`);
     });
 
-    res.json({
-      success: true,
-      frames: urls
-    });
-  });
-});
+    exec(`ffmpeg -i video.mp4 -vf fps=1 frames/frame_%03d.jpg`, (err, stdout, stderr) => {
+      if (err) {
+        console.log("FFMPEG ERROR:", stderr);
+        return res.json({ error: "FFmpeg gagal" });
+      }
 
-const urls = files.map(file => {
-  return `http://localhost:3000/frames/${file}`;
-});
+      const files = fs.readdirSync("frames");
 
-res.json({
-  success: true,
-  frames: urls
-});
+      const urls = files.map(file => {
+        return `https://video-to-image-production.up.railway.app/frames/${file}`;
+      });
+
+      res.json({
+        success: true,
+        frames: urls
+      });
     });
   });
 });
 
 app.listen(3000, () => {
-  console.log("Server jalan di http://localhost:3000");
+  console.log("Server jalan di port 3000");
 });
